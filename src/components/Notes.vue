569a33e5a5c739f50293b4cf428b7db191312254
avguschr <avguschr@gmail.com>
2022-09-21 15:09:30 +0700

    ready

--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -50,18 +50,71 @@
       <div class="text-start" v-else>Ничего не найдено</div>
     </div>
     <div v-else>
-      <div v-for="note in notes" class="notes" :key="note.id">
-        <note
-          @changeColor="changeColor"
-          @editNote="editNote"
-          @delNote="delNote"
-          @editNoteItem="editNoteItem"
-          @addItem="addNoteItem"
-          @changeItemStatus="changeItemStatus"
-          @delNoteItem="delNoteItem"
-          :note="note"
-        />
+      <div class="mb-3">
+        <select
+          @change="sortNotes"
+          v-model="selectedSort"
+          class="form-select"
+          aria-label="Default select example"
+        >
+          <option value="0" selected>Нет сортировки</option>
+          <option value="1">Сортировка по названию</option>
+          <option value="2">Сортировка по дате</option>
+        </select>
       </div>
+
+      <div v-if="selectedSort === '1'">
+        <div v-for="note in sortName" class="notes" :key="note.id">
+          <note
+            @changeColor="changeColor"
+            @editNote="editNote"
+            @delNote="delNote"
+            @editNoteItem="editNoteItem"
+            @addItem="addNoteItem"
+            @changeItemStatus="changeItemStatus"
+            @delNoteItem="delNoteItem"
+            :note="note"
+          />
+        </div>
+      </div>
+
+      <div v-else-if="selectedSort === '2'">
+        <div v-for="note in sortDate" class="notes" :key="note.id">
+          <note
+            @changeColor="changeColor"
+            @editNote="editNote"
+            @delNote="delNote"
+            @editNoteItem="editNoteItem"
+            @addItem="addNoteItem"
+            @changeItemStatus="changeItemStatus"
+            @delNoteItem="delNoteItem"
+            :note="note"
+          />
+        </div>
+      </div>
+
+      <draggable
+        v-else
+        style="padding-bottom: 7vh"
+        @end="end"
+        :list="notes"
+        group="notes"
+      >
+        <transition-group>
+          <div v-for="note in notes" class="notes" :key="note.id">
+            <note
+              @changeColor="changeColor"
+              @editNote="editNote"
+              @delNote="delNote"
+              @editNoteItem="editNoteItem"
+              @addItem="addNoteItem"
+              @changeItemStatus="changeItemStatus"
+              @delNoteItem="delNoteItem"
+              :note="note"
+            />
+          </div>
+        </transition-group>
+      </draggable>
     </div>
   </div>
 </template>
@@ -70,8 +123,9 @@ import Note from "./Note.vue";
 import { format } from "date-fns";
 import ru from "date-fns/locale/ru";
 import { v4 as uuidv4 } from "uuid";
+import { VueDraggableNext } from "vue-draggable-next";
 export default {
-  components: { Note },
+  components: { Note, draggable: VueDraggableNext },
   name: "notes-component",
   data() {
     return {
@@ -79,6 +133,7 @@ export default {
       notes: [],
       showSearchInput: false,
       searchString: "",
+      selectedSort: 0,
     };
   },
   computed: {
@@ -87,8 +142,44 @@ export default {
         (note) => note.name.indexOf(this.searchString) !== -1
       );
     },
+    sortName() {
+      return this.sortNotesName();
+    },
+    sortDate() {
+      return this.sortNotesDate();
+    },
   },
   methods: {
+    sortNotesName() {
+      const sorted = [...this.notes];
+      return sorted.sort((a, b) => {
+        if (a.name > b.name) {
+          return 1;
+        }
+        if (a.name < b.name) {
+          return -1;
+        }
+        return 0;
+      });
+    },
+    sortNotesDate() {
+      const sorted = [...this.notes];
+      return sorted.sort((a, b) => {
+        if (a.rawDate > b.rawDate) {
+          return 1;
+        }
+        if (a.rawDate < b.rawDate) {
+          return -1;
+        }
+        return 0;
+      });
+    },
+    sortNotes() {
+      console.log(this.selectedSort);
+    },
+    end() {
+      localStorage.notes = JSON.stringify(this.notes);
+    },
     changeColor(obj) {
       const noteIndex = this.notes.findIndex((note) => note.id === obj.note.id);
       this.notes[noteIndex].color = obj.newColor;
@@ -97,6 +188,7 @@ export default {
     createNote() {
       if (this.showSearchInput) {
         this.showSearchInput = false;
+        this.searchString = "";
       } else {
         if (this.noteName) {
           this.notes.unshift({
@@ -104,6 +196,7 @@ export default {
             name: this.noteName,
             color: "#ededed",
             date: format(new Date(), "d MMM y г. в H:mm", { locale: ru }),
+            rawDate: new Date(),
             items: [],
           });
           this.noteName = "";
